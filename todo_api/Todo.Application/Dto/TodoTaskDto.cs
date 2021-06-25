36762abd817fff9d465aa2fd40d37feef4a73a63
Guilherme <guih.a8@gmail.com>
2021-06-25 14:37:42 -0300

    Criacao das dtos

---
+++ todo_api/Todo.Application/Dto/TodoTaskDto.cs
@@ -0,0 +1,9 @@
+namespace Todo.Application.Dto
+{
+    public class TodoTaskDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
