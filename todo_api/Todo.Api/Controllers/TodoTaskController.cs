eb349d0c52edf3290058dbb707e2ce59e1396eec
Guilherme <guih.a8@gmail.com>
2021-06-26 09:40:28 -0300

    Criacao da controller da agenda

--- todo_api/Todo.Api/Controllers/TodoTaskController.cs
+++ todo_api/Todo.Api/Controllers/TodoTaskController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Application.Business.Interface;
+using Todo.Application.Dto.TodoTask;
 
 namespace Todo.Api.Controllers
 {
@@ -20,6 +21,36 @@ namespace Todo.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll() {
             var result = await _todoTaskBusiness.GetAllAsync();
+            
+            if (result.Success)
+                return Ok(result);
+            
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTodoTaskDto data) {
+            var result = await _todoTaskBusiness.CreateAsync(data);
+            
+            if (result.Success)
+                return Ok(result);
+            
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateTodoTaskDto data) {
+            var result = await _todoTaskBusiness.UpdateAsync(data);
+            
+            if (result.Success)
+                return Ok(result);
+            
+            return BadRequest(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove([FromQuery] int id) {
+            var result = await _todoTaskBusiness.RemoveAsync(id);
             
             if (result.Success)
                 return Ok(result);
