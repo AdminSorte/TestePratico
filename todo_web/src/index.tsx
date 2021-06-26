f85efddf66d6cd22a96e7336d1032952dc1f81ea
Guilherme <guih.a8@gmail.com>
2021-06-26 11:28:43 -0300

    Adicionado configuracoes de roteamento

--- todo_web/src/index.tsx
+++ todo_web/src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Auth } from './pages/auth/auth.page';
+import { Todo } from './pages/todo/todo.page';
+import { Provider } from 'react-redux';
+import store from './store';
+import PrivateRoute from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+		<Switch>
+		  <Provider store={store}>
+			  <Route exact path="/" component={Auth} />
+			  <PrivateRoute exact path="/todo" component={Todo} />
+		  </Provider>
+		</Switch>
+	</BrowserRouter>,
   document.getElementById('root')
 );
-
-reportWebVitals();
