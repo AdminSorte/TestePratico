0298b569bb7e6418e4c558adadebd937eb418c46
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-06 12:40:03 -0300

    fix: Home style(WIP)

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -3,29 +3,37 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-`
+
+  `
 
 export const ContentContainer = styled.div`
   min-height: 100vh;
   max-width: 1000px;
-
+  
   padding: 0 10px;
-`
+  
+  `
 
 export const Header = styled.div`
   width: 100%;
-  height: 12vh;
-
+  height: 20%;
+  max-height: 20vh;
+  
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  
+  padding: 10px;
 `
 
 export const Content = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
 
   width: 100%;
-  height: 88vh;
+  height: 80%;
+  max-height: 80vh;
+
+  padding: 10px;
 `
