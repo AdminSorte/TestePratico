01429e776797ffa757b29d39f1a346b6775870da
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-05 22:09:13 -0300

    add: Commitments mock

---
+++ src/mock/commitments.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    description: `Nostrud anim exercitation dolore ad id sunt irure do deserunt officia quis. Do qui ea quis reprehenderit tempor ipsum tempor enim nostrud cupidatat quis in. Dolore eiusmod occaecat est do elit nostrud Lorem et consequat culpa ad consequat occaecat.
+Id aliquip consequat occaecat aliqua. Reprehenderit exercitation consectetur nisi commodo velit et irure adipisicing. Tempor occaecat sint non magna duis reprehenderit proident consequat aute nostrud.`,
+    date: '2022-01-01',
+    title: 'The Celestial title',
+  },
+  {
+    id: 2,
+    title: 'The Celestial title',
+    date: '2022-01-01',
+    description: `Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
+
+Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
+  },
+]
