53579eb484676402f8ce04321fc7206a7bda4a47
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-05 22:15:19 -0300

    add: Single commitment mock

---
+++ src/mock/singleCommitment.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    description: 'Nostrud anim exercitation dolore ad id sunt irure do deserunt officia quis.',
+    date: '2022-01-01',
+    title: 'The Celestial title',
+  },
+]
