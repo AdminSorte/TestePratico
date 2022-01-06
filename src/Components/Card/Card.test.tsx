5a1572221373bb8f31c566d466c835de31f93d92
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-05 22:16:38 -0300

    fix: Card test

---
+++ src/Components/Card/Card.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import { Card } from '.'
+
+import commitmentMock from '../../mock/singleCommitment'
+
+describe('Card', () => {
+  const props = {
+    title: 'Minha agenda',
+    content: commitmentMock,
+  }
+
+  it('should render the title of the Card', () => {
+    render(<Card title={props.title} content={[]} />)
+
+    const element = screen.getByText(props.title)
+
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should render the content of the Card', () => {
+    render(<Card title={props.title} content={props.content} />)
+
+    const element = screen.getByText(props.content[0].description)
+
+    expect(element).toBeInTheDocument()
+  })
+})
