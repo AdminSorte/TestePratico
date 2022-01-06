fd8b9b805f868b115eae3efade17cfedc0f2ecbc
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-06 15:47:46 -0300

    add: CheckIcon

---
+++ src/assets/icons/CheckIcon.tsx
@@ -0,0 +1,20 @@
+const CheckIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#484848"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-check"
+    >
+      <polyline points="20 6 9 17 4 12"></polyline>
+    </svg>
+  )
+}
+
+export { CheckIcon }
