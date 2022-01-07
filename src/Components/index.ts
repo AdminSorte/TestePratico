4625ef0c7773a7bd48a507feae15f4c9d6a7efb2
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-06 21:40:20 -0300

    feat: New components

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,6 +3,17 @@ import { CreateEditCommitment } from './Cards/CreateEditCommitment'
 import { Loading } from './Animations/loading'
 import { Menu } from './Menu'
 import { Modal } from './Modal'
+import { New } from './Buttons/New/New'
+import { TaskCard } from './Cards/TaskCard'
 import { TheHeader } from './TheHeader'
 
-export { Card, Menu, CreateEditCommitment, Modal, TheHeader, Loading }
+export {
+  Card,
+  CreateEditCommitment,
+  Loading,
+  Menu,
+  Modal,
+  New,
+  TaskCard,
+  TheHeader,
+}
