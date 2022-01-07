cd186c321148c4009172969240b4c4b7e983e180
Yago Crispim Rodrigues Souza <34103098+YagoCrispim@users.noreply.github.com>
2022-01-06 21:39:36 -0300

    fix: Mock content

--- src/mock/commitments.ts
+++ src/mock/commitments.ts
@@ -2,92 +2,19 @@
 export default [
   {
     id: 1,
-    title: 'The Celestial title 31',
+    title:
+      'Amet laboris do commodo fugiat mollit non minim mollit labore qui aliqua pariatur tempor minim.',
     date: '01/01/2022',
     hour: '12:00',
     description: `The Celestial title 31 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
 Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
   },
   {
     id: 2,
-    title: 'The Celestial title 2212',
-    date: '01/01/2022',
+    title:
+      'Ut velit culpa laboris id. Laborum do do mollit labore Lorem nulla duis id.',
+    date: '01/01/2021',
     hour: '12:00',
-    description: `The Celestial title 2212 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 3,
-    title: 'The Celestial title 883',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 883 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 4,
-    title: 'The Celestial title 94',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 94 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 5,
-    title: 'The Celestial title 125',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 125 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 6,
-    title: 'The Celestial title 56',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 56 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 7,
-    title: 'The Celestial title 6547',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 6547 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 8,
-    title: 'The Celestial title 831',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 831 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 9,
-    title: 'The Celestial title 9654',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 9654 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
-  },
-  {
-    id: 10,
-    title: 'The Celestial title 1840',
-    date: '01/01/2022',
-    hour: '12:00',
-    description: `The Celestial title 1840 Est id ea laborum velit nisi est commodo anim dolor magna non velit. Aute do fugiat anim cillum et Lorem. Enim officia proident tempor fugiat tempor et irure occaecat quis est irure. Ad dolore esse sit ex nulla et aute ullamco reprehenderit id in nulla. Ex amet non minim nulla. Commodo id ex id commodo mollit officia incididunt adipisicing sunt consequat cupidatat velit culpa.
-
-Enim cupidatat ipsum ad ex eiusmod elit veniam aute ullamco aliquip reprehenderit labore enim. Tempor voluptate irure velit dolore eu qui fugiat. Velit duis pariatur aliqua aliquip. Laborum aliqua consequat ipsum Lorem commodo cupidatat nulla commodo nostrud. Laborum voluptate nulla laborum magna magna occaecat enim pariatur ea deserunt ut. Irure cillum esse aute proident excepteur exercitation dolore ipsum incididunt amet amet.`,
+    description: `Amet laboris do commodo fugiat mollit non minim mollit labore qui aliqua pariatur tempor minim. Deserunt laboris ullamco adipisicing officia do laboris est consequat. Non adipisicing nulla do fugiat aliquip culpa amet. Mollit occaecat ullamco aliqua amet nulla esse consequat in Lorem esse sint incididunt ut.`,
   },
 ]
