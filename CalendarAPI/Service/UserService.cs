b7fe9878b13a7030c46d9d1c0ed76e77eb806457
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-13 14:26:36 -0300

    feat:  interface service to connect  with UserDB

---
+++ CalendarAPI/Service/UserService.cs
@@ -0,0 +1,21 @@
+﻿using CalendarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarAPI.Service
+{
+    public class UserService
+    {
+    }
+
+    public interface IUserServiceInterface
+    {
+        TokenResponse CreateToken(AuthModel user);
+        void CreateUser(UserCreate user);
+        void DeleteUser(string email, string password);
+        string GetEmailFromToken(string token);
+        User GetUserFromToken(string token);
+    }
+}
