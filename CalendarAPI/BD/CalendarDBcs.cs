64fb9b33ed43e774fdef5d8179c7f1e7411cec25
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-13 14:49:23 -0300

    feat: create connect betwwteen calendar and DB

---
+++ CalendarAPI/BD/CalendarDBcs.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CalendarAPI.Models.CalendarModel;
+
+namespace CalendarAPI.BD
+{
+    public class CalendarDB : DbContext
+    {
+        public CalendarDB(DbContextOptions<CalendarDB> options) : base(options)
+        {
+        }
+        public DbSet<Calendar> calendar { get; set; }
+
+        public DbSet<Events> events { get; set; }
+
+        public DbSet<EventsShare> eventsShare { get; set; }
+
+
+    }
+}
