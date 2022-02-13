b53ddbf4871c7479519e00cee330887e683999b8
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-13 14:49:35 -0300

    feat: create interface to calendar user

---
+++ CalendarAPI/Service/CalendarService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CalendarAPI.Models.CalendarModel;
+
+namespace CalendarAPI.Service
+{
+    public class CalendarService
+    {
+    }
+
+
+    public interface ICalendarServiceInterface
+    {
+        List<Events> GetAllEventsUser(string token);
+        Events CreateEvent(string token, Events newEvent);
+        void DeleteEvent(string token, string id);
+        List<Events> GetAllEventsInInterval(string token, DateTime startDate);
+        Calendar GetCalendar(string token, string id);
+        Calendar CreateCalendar(string token, string name);
+        void DeleteCalendar(string token, string id);
+        Calendar CreateCalendar(string token, Calendar calendar);
+        List<Calendar> GetAllCalendars(string token);
+
+    }
+}
