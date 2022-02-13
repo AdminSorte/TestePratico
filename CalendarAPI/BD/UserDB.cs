56f23174365a3b862746a5b2610c911de36bcd15
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-13 14:32:48 -0300

    feat: modify USERDB

---
+++ CalendarAPI/BD/UserDB.cs
@@ -0,0 +1,33 @@
+﻿using CalendarAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarAPI.BD
+{
+    public class UserDB : DbContext
+    {
+
+        public UserDB(DbContextOptions<UserDB> options) : base(options)
+        {
+        }
+        public DbSet<User> users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    username = "root",
+                    password = "root",
+                    dateCreated = DateTime.Now,
+                    email = "root"
+
+                }
+              );
+        }
+    }
+}
