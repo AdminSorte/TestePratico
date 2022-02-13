00a7d174a229fd7734a97b43b817315534dba0eb
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-13 14:29:44 -0300

    feat: create user model DBContext

--- CalendarAPI/UserDB.cs
+++ CalendarAPI/UserDB.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using CalendarAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CalendarAPI
 {
-    public class UserDB
+    public class UserDB : DbContext
     {
+
+        public UserDB(DbContextOptions<UserDB> options) : base(options)
+        {
+        }
+        public DbSet<User> users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    username = "root",
+                    password = "root",
+                    dateCreated = DateTime.Now,
+                    email = "root"
+
+                }
+              );
+
+        }
     }
 }
