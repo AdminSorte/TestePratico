032e4f636b693a0d2f930e3385249258047dae23
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-02-14 05:28:02 -0300

    feat: create static method to mock calendarservice

---
+++ CalendarTest/Mocks/UserServiceMock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static CalendarAPI.Models.CalendarAPIModel;
+
+namespace CalendarTest.Mocks
+{
+    public class UserServiceMock
+    {
+
+        public static List<EventResponse> GetEventDependUser(string token)
+        {
+            if(token.Replace("Bearer","").Trim() == "0.0.0.1")
+            {
+                var eventFake = new EventResponse() { id = "0.0.0.1" };
+                var events = new List<EventResponse>();
+                events.Add(eventFake);
+                return events;
+            }
+            return new List<EventResponse>();
+        }
+
+        public static List<EventResponse> GetFailDependUser(string token)
+        {
+            if (!(token.Replace("Bearer", "").Trim() == "0.0.0.1"))
+            {
+                var eventFake = new EventResponse() { id = "0.0.0.1" };
+                var events = new List<EventResponse>();
+                events.Add(eventFake);
+                return events;
+            }
+            throw new Exception("Error");
+        }
+    }
+}
